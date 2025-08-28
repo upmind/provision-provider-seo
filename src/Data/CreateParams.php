dbed91f1f15fc2b60507a4c5ee0b65ea6f941004
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2025-08-28 14:20:07 +0300

    Added customer address & customer phone as nullable attributes in the create params

--- src/Data/CreateParams.php
+++ src/Data/CreateParams.php
@@ -11,6 +11,8 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
  * @property-read mixed $customer_id Id of the customer in the billing system
  * @property-read string $customer_email Email address of the customer
  * @property-read string|null $customer_name Name of the customer
+ * @property-read string|null $customer_phone Phone number of the customer in international format
+ * @property-read CustomerAddressParams|null $customer_address Address of the customer
  * @property-read string $domain Domain name the account is for
  * @property-read string $package_identifier Service package identifier, if any
  * @property-read string[]|null $promo_codes Optional array of promo codes applied to the order
@@ -24,6 +26,8 @@ class CreateParams extends DataSet
             'customer_id' => ['required'],
             'customer_email' => ['required', 'email'],
             'customer_name' => ['nullable', 'string'],
+            'customer_phone' => ['nullable', 'string', 'international_phone'],
+            'customer_address' => ['nullable', CustomerAddressParams::class],
             'domain' => ['required', 'string', 'alpha-dash-dot'],
             'package_identifier' => ['required', 'string'],
             'promo_codes' => ['nullable', 'array'],
