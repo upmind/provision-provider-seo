ac36bbbf7b31c30c5b96762feefe27738f37565f
Elena Filippova <elena.filippova@codeline.fi>
2025-10-02 13:12:05 +0300

    Implement RankingCoach

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -7,6 +7,7 @@ namespace Upmind\ProvisionProviders\Seo;
 use Upmind\ProvisionBase\Laravel\ProvisionServiceProvider;
 use Upmind\ProvisionProviders\Seo\Providers\Example\Provider as ExampleProvider;
 use Upmind\ProvisionProviders\Seo\Providers\Marketgoo\Provider as MarketgooProvider;
+use Upmind\ProvisionProviders\Seo\Providers\RankingCoach\Provider as RankingCoachProvider;
 
 class LaravelServiceProvider extends ProvisionServiceProvider
 {
@@ -17,5 +18,6 @@ class LaravelServiceProvider extends ProvisionServiceProvider
         // $this->bindProvider('seo', 'example', ExampleProvider::class);
 
         $this->bindProvider('seo', 'marketgoo', MarketgooProvider::class);
+        $this->bindProvider('seo', 'ranking-coach', RankingCoachProvider::class);
     }
 }
